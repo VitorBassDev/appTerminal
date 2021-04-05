a5ab90d0aa65b33f7e953599da069c76e2f883df
Vitor Guedes <vittorbassdev@gmail.com>
2021-04-05 11:41:44 -0300

    Add initialst to terminal controller

---
+++ client/src/terminalController.js
@@ -0,0 +1,17 @@
+/**
+ * CONTROLADOR
+ * RECEBER AS REGRAS DE NEGÓCIO 
+ * DELEGAR OS EVENTOS
+ * RECEBER MENSAGENS
+ */
+
+export default class TerminalController{
+  constructor(){
+
+  }
+
+  // Starta o Projeto - initalizeTable
+  async initalizeTable(eventEmitter){
+    console.log("Inicializou")
+  }
+}
